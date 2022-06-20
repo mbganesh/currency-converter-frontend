820b72711513a7f83b29d69c27c599fe9e55a828
mbganesh <crazyganesh179@gmail.com>
2022-06-20 22:25:44 +0530

    Test Mode

--- src/constants/APIClient.js
+++ src/constants/APIClient.js
@@ -1,7 +1,9 @@
 export default {
   API_PRODUCTION: 'http://192.168.1.26:3002/api',
 
-  API_DEVELOPMENT : 'http://localhost:8000/',
+  API_DEVELOPMENT : 'http://localhost:8000',
+
+  CONVERT_API_TEST : 'https://mbganesh-currency-converter.herokuapp.com/convert'
 
 };
 
